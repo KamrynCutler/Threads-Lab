f0439466577811cae46df3ae8fcdfa4460ce6713
KamrynCutler <Kammykam0899@gmail.com>
2020-12-13 17:07:10 -0500

    Update sudoku.c

--- Thread-Lab-main/Threads Lab/sudoku/sudoku.c
+++ Thread-Lab-main/Threads Lab/sudoku/sudoku.c
@@ -7,42 +7,31 @@
 
 // Sudoku board. Modify this to test your checker!
 // 0 == empty cell; 1-9 is the filled in digit.
-int board[SIZE][SIZE] = {
-  {2, 2, 4, 5, 3, 9, 6, 8, 7},
+int  board[SIZE][SIZE] = {
+  {2, 8, 5, 4, 7, 3, 9, 1, 6},
+  {4, 9, 6, 1, 8, 2, 5, 7, 3},
+  {3, 7, 1, 9, 5, 6, 8, 4, 2},
+  {7, 6, 2, 3, 9, 1, 4, 5, 8},
+  {9, 5, 8, 2, 4, 7, 3, 6, 1},
+  {1, 4, 3, 8, 6, 5, 7, 2, 9},
+  {8, 3, 7, 6, 1, 4, 2, 9, 5},
   {5, 1, 9, 7, 2, 8, 6, 3, 4},
-  {8, 3, 9, 6, 1, 4, 2, 9, 5},
-  {1, 4, 3, 9, 6, 5, 7, 2, 9},
-  {5, 5, 8, 2, 4, 7, 3, 6, 1},
-  {7, 6, 2, 3, 9, 7, 4, 5, 8},
-  {3, 7, 1, 9, 5, 6, 8, 5, 2},
-  {4, 2, 6, 1, 8, 2, 5, 7, 4},
-  {2, 8, 2, 4, 7, 3, 9, 1, 6}
+  {6, 2, 4, 5, 3, 9, 1, 8, 7}
 };
-//correct
-// int board[SIZE][SIZE] = {
-//   {6, 2, 4, 5, 3, 9, 1, 8, 7},
-//   {5, 1, 9, 7, 2, 8, 6, 3, 4},
-//   {8, 3, 7, 6, 1, 4, 2, 9, 5},
-//   {1, 4, 3, 8, 6, 5, 7, 2, 9},
-//   {9, 5, 8, 2, 4, 7, 3, 6, 1},
-//   {7, 6, 2, 3, 9, 1, 4, 5, 8},
-//   {3, 7, 1, 9, 5, 6, 8, 4, 2},
-//   {4, 9, 6, 1, 8, 2, 5, 7, 3},
-//   {2, 8, 5, 4, 7, 3, 9, 1, 6}
-// };
 
 // array of bools
-bool check_row[SIZE], check_col[SIZE], check_box[SIZE]; // one for each box
+bool row_check[SIZE], col_check[SIZE], box_check[SIZE];
 
-typedef struct {
+typedef struct
+{
   int row, col, box;
-} check_data;
-int i,j;
+} check;
+
 
 //Pring board fucntion
 void printBoard(int board[SIZE][SIZE]) {
-  for(i = 0; i < SIZE; i++) {
-    for(j = 0; j < SIZE; j++) {
+  for(int i = 0; i<SIZE; i++) {
+    for(int j = 0; j<SIZE; j++) {
       printf("%5d", board[i][j]);
     }
     printf("\n");
@@ -50,94 +39,90 @@ void printBoard(int board[SIZE][SIZE]) {
   printf("\n");
 }
 
-// Checks the given row for duplicate numbers, and updates the check_row
+// Checks the given row for duplicate numbers, and updates the row_check
 // value for that row appropriately. If no number is repeated in that row,
-// check_row[row] will be set to true; otherwise, it will be false.
+// row_check[row] will be set to true; otherwise, it will be false.
 void* checkRow(void* args) {
-  check_data * data = (check_data *) args;
+  check * data = (check *) args;
   int row = data->row;
   int col = data->col;
-  int  sum = 0;
-  for (int i = row; i < SIZE; ++i) {
+  int sum = 0;
+  int i = 0;
+  int j = 0;
+  for (i = row; i < SIZE; ++i) {
     sum = 0;
-    for (int j = col; j < SIZE; ++j) {
+    for (j = col; j < SIZE; ++j) {
       int val = board[i][j];
       sum+= val;
     }
     if (sum != 45){
-      check_row[i] = false;
+      row_check[i] = false;
     }else if(sum == 45){
-      check_row[i] = true;
+      row_check[i] = true;
     }
   }
-return  check_row;
+return row_check;
 }
 
 
-// Checks the given col for duplicate numbers, and updates the check_col
+// Checks the given col for duplicate numbers, and updates the col_check
 // value for that col appropriately. If no number is repeated in that col,
-// check_col[col] will be set to true; otherwise, it will be false.
+// col_check[col] will be set to true; otherwise, it will be false.
 void* checkCol(void* args) {
-  check_data * data = (check_data *) args;
+  check * data = (check *) args;
   int row = data->row;
   int col = data->col;
   int sum = 0;
-  int val;
-
   for (int i = col; i < SIZE; ++i) {
     sum = 0;
     for (int j = row; j < SIZE; ++j) {
-      val = board[i][j];
-      sum += val;
-    }
-    if (sum != 45){
-      check_col[i] = false;
-    } else if(sum == 45){
-      check_col[i] = true;
-    }
-  }
-  return check_col;
-}
-
-// // Checks the given 3x3 box for duplicate numbers, and updates the check_box
-// // value for that box appropriately. If no number is repeated in that box,
-// // check_box[box] will be set to true; otherwise, it will be false.
-void* checkBox(void* args) {
-  check_data * data = (check_data *) args;
-  int row = data->row;
-  int col = data->col;
-  int box_num = data->box;
-  int sum = 0;
-  int val;
-
-  for (int i = row; i < row + 3; ++i) {
-    for (int j = col; j < col + 3; ++j) {
-      val = board[i][j];
+      int val = board[i][j];
       sum+= val;
     }
     if (sum != 45){
-      check_box[box_num] = false;
+      col_check[i] = false;
     }else if(sum == 45){
-      check_box[box_num] = true;
+      col_check[i] = true;
     }
   }
-  return check_box;
+  return col_check;
 }
 
+// // Checks the given 3x3 box for duplicate numbers, and updates the box_check
+// // value for that box appropriately. If no number is repeated in that box,
+// // box_check[box] will be set to true; otherwise, it will be false.
+void* checkBox(void* args) {
+  check * data = (check *) args;
+  int row = data->row;
+  int col = data->col;
+  int boxnum = data->box;
+  int  sum = 0;
+  for (int i = row; i < row + 3; ++i) {
+    for (int j = col; j < col + 3; ++j) {
+      int val = board[i][j];
+      sum+= val;
+    }
+    if (sum != 45){
+      box_check[boxnum] = false;
+    }else if(sum == 45){
+      box_check[boxnum] = true;
+    }
+  }
+  return box_check;
+}
 
 // Spawn a thread to fill each cell in each result matrix.
 // How many threads will you spawn?
 int main() {
 
   //args for column and row checks
-  check_data * check_row_col = (check_data *) malloc(sizeof(check_data));
-  check_row_col->row = 0;
-  check_row_col->col = 0;
+  check * row_col_check = (check *) malloc(sizeof(check));
+  row_col_check->row = 0;
+  row_col_check->col = 0;
 
-  check_data * boxes[SIZE];
-  for (size_t i = 0; i < SIZE; i++) { 
-    // memory  allocation
-    boxes[i] = (check_data *) malloc(sizeof(check_data));
+  check * boxes[SIZE];
+  for (size_t i = 0; i < SIZE; i++) { // memory  allocation
+    boxes[i] = (check *) malloc(sizeof(check));
   }
   //box 1 0,0
   boxes[0]->row = 0;
@@ -194,26 +179,29 @@ int main() {
   pthread_t rows,cols, box[SIZE];
 
   //return vales for threads
-  void *all_rows;
-  void *all_cols;
-  void *returnbox1;
-  void *returnbox2;
-  void *returnbox3;
-  void *returnbox4;
-  void *returnbox5;
-  void *returnbox6;
-  void *returnbox7;
-  void *returnbox8;
-  void *returnbox9;
+  void * all_rows;
+  void * all_cols;
+  void * returnbox1;
+  void * returnbox2;
+  void * returnbox3;
+  void * returnbox4;
+  void * returnbox5;
+  void * returnbox6;
+  void * returnbox7;
+  void * returnbox8;
+  void * returnbox9;
+
 
   // 3. Create a thread for each cell of each matrix operation.
-  pthread_create(&rows, NULL, checkRow, (void *) check_row_col);
-  pthread_create(&cols, NULL, checkCol, (void *) check_row_col);
+  pthread_create(&rows, NULL, checkRow, (void *) row_col_check);
+  pthread_create(&cols, NULL, checkCol, (void *) row_col_check);
   for (size_t i = 0; i < SIZE; i++) {
     pthread_create(&box[i], NULL, checkBox, (void *) boxes[i]);
   }
 
+
   // 4. Wait for all threads to finish.
+  //row_check
   pthread_join(rows, &all_rows);
   pthread_join(cols, &all_cols);
 
@@ -229,39 +217,39 @@ int main() {
 
   // 5. Print the results.
   printf("Results:\n");
-  bool pass_all_rows = true;
+  bool all_rows_passed = true;
   printf("Rows:\n");
   for (int i = 0; i < SIZE; i++) {
-    if (!check_row[i]) {
-      printf("Row %i failed\n", i+1);
-      pass_all_rows = false;
+    if (!row_check[i]) {
+      printf("Row %i did not pass\n", i+1);
+      all_rows_passed = false;
     }
   }
-  if (pass_all_rows) {
+  if (all_rows_passed) {
     printf("All rows passed!\n");
   }
 
-  bool pass_all_cols = true;
+  bool all_cols_passed = true;
   printf("Cols:\n");
   for (int i = 0; i < SIZE; i++) {
-    if (!(bool)check_col[i]) {
-      printf("Col %i failed\n", i+1);
-      pass_all_cols = false;
+    if (!(bool)col_check[i]) {
+      printf("Col %i did not pass\n", i+1);
+      all_cols_passed = false;
     }
   }
-  if (pass_all_cols) {
+  if (all_cols_passed) {
     printf("All cols passed!\n");
   }
 
-  bool pass_all_boxes = true;
+  bool all_boxes_passed = true;
   printf("Boxes:\n");
   for (int i = 0; i < SIZE; i++) {
-    if (!(bool)check_box[i]) {
-      printf("Box %i failed\n", i+1);
-      pass_all_boxes = false;
+    if (!(bool)box_check[i]) {
+      printf("Box %i did not pass\n", i+1);
+      all_boxes_passed = false;
     }
   }
-  if (pass_all_boxes) {
+  if (all_boxes_passed) {
     printf("All boxes passed!\n");
   }
   return 0;
